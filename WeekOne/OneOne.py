7e9c9b645cc6d66cacf726e1b4ac22142e92cff2
Daddy Ousen <rhasan228@gmail.com>
2022-02-05 05:27:05 +0600

    init

---
+++ WeekOne/OneOne.py
@@ -0,0 +1,16 @@
+from unicodedata import name
+
+
+print("hello world")
+print("hello again")
+print("I like typing this.")
+print("This is fun.")
+
+name = "Brook"
+print("Hello, " + name + "!")
+print(4+5)
+print(4-5)
+print(4*5)
+print(4/5)
+print(4//5)
+print((((4+5)**2)**3)**(1/2))
