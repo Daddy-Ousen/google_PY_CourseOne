f32d3e8c183face26759211acdbaf5713438b9da
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 17:29:57 +0600

    mmm

--- WeekTwo/FileTwoVariables.py
+++ WeekTwo/FileTwoVariables.py
@@ -18,3 +18,12 @@ amount_a = get_seconds(2,30,0)
 amount_b = get_seconds(0,45,15)
 result = amount_a + amount_b
 print(result)
+
+def convert_seconds(secondss):
+    hours = secondss // 3600
+    minutes = (secondss - hours * 3600) // 60
+    seconds = secondss - hours * 3600 - minutes * 60
+    return hours, minutes, seconds
+
+hours, minutes, seconds = convert_seconds(5000)
+print(hours, minutes, seconds)
