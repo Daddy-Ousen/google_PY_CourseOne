4fa474d8901315f5cff0bd8f398a0cc7f31b98e8
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 17:21:59 +0600

    mm

--- WeekTwo/FileTwoVariables.py
+++ WeekTwo/FileTwoVariables.py
@@ -10,3 +10,11 @@ area_a = area(10, 2)
 area_b = area(20, 2)
 sum = area_a + area_b
 print("the sum of the areas is " + str(sum))
+
+def get_seconds(hours, minutes, seconds):
+  return 3600*hours + 60*minutes + seconds
+
+amount_a = get_seconds(2,30,0)
+amount_b = get_seconds(0,45,15)
+result = amount_a + amount_b
+print(result)
