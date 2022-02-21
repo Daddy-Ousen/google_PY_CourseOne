176d84d5d815f2d1ee021a249e016fbe0f33ffb4
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 17:32:38 +0600

    mm

---
+++ WeekTwo/FileFourCodeReuse.py
@@ -0,0 +1,10 @@
+name = "mia"
+number = len(name) * 9
+
+print("Hello, " + name + "! Your number is " + str(number))
+
+name = "Carol"
+number = len(name) * 9
+
+print("Hello, " + name + "! Your number is " + str(number))
+
