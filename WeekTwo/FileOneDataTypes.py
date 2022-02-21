1a54d650b225e6de6a7593dba8104ab830254612
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 16:54:38 +0600

    commit on main

---
+++ WeekTwo/FileOneDataTypes.py
@@ -0,0 +1,8 @@
+print(type("hello"))
+print(type(5))
+print(type(5.3))
+print(type(True))
+print(type(None))
+print(type(print))
+print(type(type))
+print(type(type("hello")))
---
+++ WeekTwo/FileTwoVariables.py
@@ -0,0 +1,4 @@
+length = 10
+width = 2
+area = length * width
+print("the area is " + str(area))
