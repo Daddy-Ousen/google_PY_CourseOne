8f361a00a2b588184ee8452350acc93069eaf1d5
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 17:16:43 +0600

    comm

--- WeekTwo/FileThreeDefineFunction.py
+++ WeekTwo/FileThreeDefineFunction.py
@@ -3,3 +3,10 @@ def greeting(name, department):
     print("You are part of " + department)
 
 greeting("Brook", "IT")
+
+def print_seconds(hours, minutes, seconds):
+    print(3600 * (hours))
+    print(60 * (minutes))
+    print(seconds)
+
+print_seconds(1,2,3)
