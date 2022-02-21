0e8d7aeddd7d4c53278dc630ed6d467e4886d70c
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 17:19:48 +0600

    mm

--- WeekTwo/FileTwoVariables.py
+++ WeekTwo/FileTwoVariables.py
@@ -2,3 +2,11 @@ length = 10
 width = 2
 area = length * width
 print("the area is " + str(area))
+
+def area(length, width):
+    return length * width
+
+area_a = area(10, 2)
+area_b = area(20, 2)
+sum = area_a + area_b
+print("the sum of the areas is " + str(sum))
