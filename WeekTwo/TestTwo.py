44c16ea4abc8c36d01d3f00571ef8243d92f2381
Daddy-Ousen <rhasan228@gmail.com>
2022-02-21 17:05:58 +0600

    commit on main

---
+++ WeekTwo/TestTwo.py
@@ -0,0 +1,21 @@
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(str(result))
+
+
+word1 = "How "
+word2 = "do "
+word3 = "you "
+word4 = "like "
+word5 = "Python "
+word6 = "so "
+word7 = "far?"
+
+print(word1+word2+word3+word4+word5+word6+word7)
+
+
+result = 2+2
+print(str("2 + 2 = ") + str(result))
+
+
